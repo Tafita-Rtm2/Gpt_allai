bc95b533d9f359154c3a9ebae9c68e82906bda2f
Tafitaniaina rtm <harenabainina@gmail.com>
2025-08-19 19:34:53 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ require('dotenv').config();
 
 const express = require('express');
 const axios = require('axios');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(cors()); // Enable CORS for all routes
 
 // --- CONFIGURATION ---
 const PORT = process.env.PORT || 3000;
